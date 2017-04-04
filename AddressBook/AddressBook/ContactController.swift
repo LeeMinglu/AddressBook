fb1dbc5a0b1fdae4b552a476e48de83c3ba8cafd
LeeMinglu <402007353@qq.com>
2017-04-04 16:57:25 +0800

    创建静态单元格，需要 将dataSource代理方法去掉，否则显示不出来

---
+++ AddressBook/AddressBook/ContactController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactController.swift
+//  AddressBook
+//
+//  Created by 李明禄 on 2017/4/4.
+//  Copyright © 2017年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+class ContactController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+}
