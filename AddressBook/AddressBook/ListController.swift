11b8c30a2cecf299dd8467b392df772677e0e2a5
LeeMinglu <402007353@qq.com>
2017-04-15 09:13:18 +0800

    加载数据到tableViewCell上

--- AddressBook/AddressBook/ListController.swift
+++ AddressBook/AddressBook/ListController.swift
@@ -9,87 +9,65 @@
 import UIKit
 
 class ListController: UITableViewController {
+    
 
+    var PersonList = [Person]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData { (list) in
+            self.PersonList += list
+            print(self.PersonList)
+             //加载数据
+            self.tableView.reloadData()
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func loadData (completion: @escaping (_ result:[Person])->()) {
+        DispatchQueue.global().async {
+           
+            print("正在努力加载中")
+            
+            var personList: [Person] = []
+            
+            for i in 0..<20 {
+                let person = Person()
+                person.name = "rock-\(i)"
+                
+                let phone = "186" + String(format: "\(arc4random_uniform(100000))" )
+                person.phone = phone
+                person.address = "beijing"
+                
+                personList.append(person)
+            }
+            
+            DispatchQueue.main.async {
+                completion(personList)
+            }
+            
+        }
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+ // MARK: 代理及数据源方法
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.PersonList.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+       let cell = tableView.dequeueReusableCell(withIdentifier: "list", for: indexPath)
+//        if cell == nil {
+//            cell = UITableViewCell.init(style: .default, reuseIdentifier: "list")
+//        }
+        let person = self.PersonList[indexPath.row]
+        
+        cell.textLabel?.text = person.name
+        cell.detailTextLabel?.text = person.phone
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
---
+++ AddressBook/AddressBook/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  AddressBook
+//
+//  Created by 李明禄 on 2017/4/4.
+//  Copyright © 2017年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String?
+    var phone: String?
+    var address: String?
+
+}
